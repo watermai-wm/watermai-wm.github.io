bd707dfbef1499ce71773294878ca0c9430ee24d
watermai-wm <akasukilee@gmail.com>
2025-04-14 09:05:17 +0800

    新增產生構築文字檔功能

--- deckbuild.js
+++ deckbuild.js
@@ -832,3 +832,78 @@ window.onload = function() {
         shareButton.onclick = shareDeckAsImage;
     }
 };
+
+//產生構築文字檔
+function generateDeckText() {
+    let output = "";
+
+    // 旗艦區
+    const flagshipIDs = Object.keys(deck.flagship);
+    if (flagshipIDs.length > 0) {
+        output += "旗艦卡編號\t旗艦卡名稱\n";
+        flagshipIDs.forEach(id => {
+            const card = allCards.find(c => c.card_id == id);
+            const cleanCode = card.card_code.replace(/<.*?>/g, ""); // 去除 <>
+            output += `${cleanCode}\t${card.card_name}\n`;
+        });
+    }
+
+    // 一般卡牌
+    output += "\n卡牌編號\t卡牌名稱\n";
+    let lineNumber = 1;
+    const entries = Object.entries(deck.mainDeck);
+
+    entries.forEach(([cardID, count]) => {
+        const card = allCards.find(c => c.card_id == cardID);
+        const cleanCode = card.card_code.replace(/<.*?>/g, "");
+        for (let i = 0; i < count; i++) {
+            output += `${lineNumber}\t${cleanCode}\t${card.card_name}\n`;
+            lineNumber++;
+        }
+    });
+
+    // 文字區塊
+    let textBox = document.getElementById("deckTextBox");
+    if (!textBox) {
+        textBox = document.createElement("textarea");
+        textBox.id = "deckTextBox";
+        textBox.style.width = "100%";
+        textBox.style.height = "400px";
+        textBox.style.marginTop = "20px";
+        textBox.style.whiteSpace = "pre";
+        textBox.readOnly = true;
+        document.body.appendChild(textBox);
+    }
+    textBox.value = output;
+
+    // 複製按鈕
+    let copyBtn = document.getElementById("copyDeckTextBtn");
+    if (!copyBtn) {
+        copyBtn = document.createElement("button");
+        copyBtn.id = "copyDeckTextBtn";
+        copyBtn.innerText = "複製文字";
+        copyBtn.style.marginRight = "10px";
+        copyBtn.onclick = () => {
+            textBox.select();
+            document.execCommand("copy");
+            showSoftAlert("已複製到剪貼簿！");
+        };
+        document.body.appendChild(copyBtn);
+    }
+
+    // 下載按鈕
+    let downloadBtn = document.getElementById("downloadDeckTextBtn");
+    if (!downloadBtn) {
+        downloadBtn = document.createElement("button");
+        downloadBtn.id = "downloadDeckTextBtn";
+        downloadBtn.innerText = "下載成 .txt";
+        downloadBtn.onclick = () => {
+            const blob = new Blob([textBox.value], { type: "text/plain;charset=utf-8" });
+            const link = document.createElement("a");
+            link.href = URL.createObjectURL(blob);
+            link.download = "deck_build.txt";
+            link.click();
+        };
+        document.body.appendChild(downloadBtn);
+    }
+}
