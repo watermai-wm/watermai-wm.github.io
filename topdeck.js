cb18cb62ade4306250622f5e5a612a2279afa4dd
watermai-wm <akasukilee@gmail.com>
2025-06-14 23:08:12 +0800

    更新

--- topdeck.js
+++ topdeck.js
@@ -46,6 +46,15 @@ const topDecks = [
     "Champion": 0
   },
   {
+    "deck_id": "20250614-6",
+    "L_name": "普利茅斯",
+    "Format": "7.0",
+    "Description": "20250614 彰化鬥陣桌遊 店家賽 10人 冠軍：普利茅斯 奶綠",
+    "Level": "常規賽",
+    "Players": 10,
+    "Champion": 1
+  },
+  {
     "deck_id": "20250607-1",
     "L_name": "羅恩",
     "Format": "7.0",
