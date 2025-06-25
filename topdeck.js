a49cfbca003e28af2136a313a81cf17666c7561f
watermai-wm <akasukilee@gmail.com>
2025-06-25 21:54:33 +0800

    更新上位

--- topdeck.js
+++ topdeck.js
@@ -1,6 +1,15 @@
 // topdeck.js
 const topDecks = [
   {
+    "deck_id": "20250625-1",
+    "L_name": "明石",
+    "Format": "7.0",
+    "Description": "20250625 鬥樂-一中店 店家賽 11人 冠軍：明石 Diego",
+    "Level": "常規賽",
+    "Players": 11,
+    "Champion": 1
+  },
+  {
     "deck_id": "20250622-1",
     "L_name": "曙光",
     "Format": "7.0",
