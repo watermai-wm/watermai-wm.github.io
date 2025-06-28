30edd31d0de85df33d5900ec9f270ea271e1314b
watermai-wm <akasukilee@gmail.com>
2025-06-28 17:55:01 +0800

    更新上位

--- topdeck.js
+++ topdeck.js
@@ -1,6 +1,24 @@
 // topdeck.js
 const topDecks = [
   {
+    "deck_id": "20250628-1",
+    "L_name": "聖女貞德",
+    "Format": "7.0",
+    "Description": "20250628 貓腳印-東門店 店家賽 15人 冠軍：聖女貞德 海綿體大戰括約肌",
+    "Level": "常規賽",
+    "Players": 15,
+    "Champion": 1
+  },
+  {
+    "deck_id": "20250628-2",
+    "L_name": "普利茅斯",
+    "Format": "7.0",
+    "Description": "20250628 彰化鬥陣桌遊 店家賽 6人 冠軍：普利茅斯 政勳",
+    "Level": "常規賽",
+    "Players": 6,
+    "Champion": 1
+  },
+  {
     "deck_id": "20250626-1",
     "L_name": "曙光",
     "Format": "7.0",
