d18fa225799f5bfcc3ba01f0a695cee68ecf3a3e
watermai-wm <akasukilee@gmail.com>
2025-07-02 21:56:13 +0800

    更新

--- topdeck.js
+++ topdeck.js
@@ -1,6 +1,15 @@
 // topdeck.js
 const topDecks = [
   {
+    "deck_id": "20250702-1",
+    "L_name": "聖女貞德",
+    "Format": "7.0",
+    "Description": "20250702 鬥樂-一中店 店家賽 10人 冠軍：聖女貞德 允儀",
+    "Level": "常規賽",
+    "Players": 10,
+    "Champion": 1
+  },
+  {
     "deck_id": "20250629-1",
     "L_name": "明石",
     "Format": "7.0",
