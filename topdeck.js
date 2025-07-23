e836ac1595769d5a5559e00329f013111c7a506d
watermai-wm <akasukilee@gmail.com>
2025-07-23 22:30:48 +0800

    更新

--- topdeck.js
+++ topdeck.js
@@ -1,6 +1,15 @@
 // topdeck.js
 const topDecks = [
   {
+    "deck_id": "20250723-1",
+    "L_name": "普利茅斯",
+    "Format": "7.0",
+    "Description": "20250723 鬥樂-一中店 店家賽 11人 冠軍：普利茅斯 仲文",
+    "Level": "常規賽",
+    "Players": 11,
+    "Champion": 1
+  },
+  {
     "deck_id": "20250720-1",
     "L_name": "奧古斯特·馮·帕塞瓦爾",
     "Format": "7.0",
