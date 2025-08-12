b04cc4fa8870de9bccdb9ed9242eec307ea6b220
watermai-wm <akasukilee@gmail.com>
2025-08-12 21:59:16 +0800

    更新

--- topdeck.js
+++ topdeck.js
@@ -1,6 +1,15 @@
 // topdeck.js
 const topDecks = [
   {
+    "deck_id": "20250812-1",
+    "L_name": "羅恩",
+    "Format": "7.0",
+    "Description": "20250812 花蓮玩具e哥 玩家自辦卡套賽 8人 冠軍 羅恩 心皓",
+    "Level": "常規賽",
+    "Players": 8,
+    "Champion": 1
+  },
+  {
     "deck_id": "20250810-1",
     "L_name": "射水魚",
     "Format": "7.0",
