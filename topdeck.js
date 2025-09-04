e33345f12a77bb9d603449a406db5e6af6ec63ec
watermai-wm <akasukilee@gmail.com>
2025-09-04 22:58:11 +0800

    更新

--- topdeck.js
+++ topdeck.js
@@ -1,6 +1,15 @@
 // topdeck.js
 const topDecks = [
   {
+    "deck_id": "20250904-1",
+    "L_name": "奧古斯特·馮·帕塞瓦爾",
+    "Format": "7.0",
+    "Description": "20250904 貓腳印-東門店 休閒賽 5人 冠軍：奧古斯特·馮·帕塞瓦爾 被魔女抓回去的指揮官",
+    "Level": "常規賽",
+    "Players": 5,
+    "Champion": 1
+  },
+  {
     "deck_id": "20250831-1",
     "L_name": "明石",
     "Format": "7.0",
