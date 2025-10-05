24fd2954966281bb8e06a8d0052b6f655c62c4a2
watermai-wm <akasukilee@gmail.com>
2025-10-05 08:57:42 +0800

    更新

--- topdeck.js
+++ topdeck.js
@@ -1,6 +1,15 @@
 // topdeck.js
 const topDecks = [
   {
+    "deck_id": "20251004-1",
+    "L_name": "曙光",
+    "Format": "7.0",
+    "Description": "20251004 CardMaster 店家賽 3人 冠軍 曙光 谷風天音的哥哥",
+    "Level": "常規賽",
+    "Players": 3,
+    "Champion": 1
+  },
+  {
     "deck_id": "20250928-1",
     "L_name": "約克鎮II",
     "Format": "7.0",
