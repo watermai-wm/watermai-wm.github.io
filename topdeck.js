89746c7e9547d439367208bd9c4caa299ab4a4c0
watermai-wm <akasukilee@gmail.com>
2025-10-20 15:32:36 +0800

    更新

--- topdeck.js
+++ topdeck.js
@@ -1,6 +1,15 @@
 // topdeck.js
 const topDecks = [
   {
+    "deck_id": "20251019-1",
+    "L_name": "金剛",
+    "Format": "7.0",
+    "Description": "20251019 花蓮玩具e哥 玩家自辦卡套賽 8人 冠軍 金剛 心皓",
+    "Level": "常規賽",
+    "Players": 8,
+    "Champion": 1
+  },
+  {
     "deck_id": "20251012-1",
     "L_name": "曙光",
     "Format": "7.0",
