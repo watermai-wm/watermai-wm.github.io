2647cbf267019384e12060059c29c94396147ac1
watermai-wm <akasukilee@gmail.com>
2025-12-07 19:57:46 +0800

    更新上位

--- topdeck.js
+++ topdeck.js
@@ -1,6 +1,15 @@
 // topdeck.js
 const topDecks = [
   {
+    "deck_id": "20251207-1",
+    "L_name": "魯普雷希特親王",
+    "Format": "7.5.1",
+    "Description": "20251207 貓腳印-士林店 桌墊賽 9人 冠軍 : 魯普雷希特親王 水麥",
+    "Level": "常規賽",
+    "Players": 9,
+    "Champion": 1
+  },
+  {
     "deck_id": "20251206-1",
     "L_name": "天城",
     "Format": "7.5.1",
