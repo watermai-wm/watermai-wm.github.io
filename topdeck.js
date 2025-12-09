5c2c5dcb21d276ff7934a5e018bfb378eb5b4e53
watermai-wm <akasukilee@gmail.com>
2025-12-09 22:31:46 +0800

    更新上位

--- topdeck.js
+++ topdeck.js
@@ -1,6 +1,15 @@
 // topdeck.js
 const topDecks = [
   {
+    "deck_id": "20251209-1",
+    "L_name": "哈爾濱",
+    "Format": "7.5.1",
+    "Description": "20251209 玩具e哥花蓮和平店 碧藍戰卡CE週賽 6人 冠軍 哈爾濱 藍玥",
+    "Level": "常規賽",
+    "Players": 6,
+    "Champion": 1
+  },
+  {
     "deck_id": "20251207-2",
     "L_name": "曙光",
     "Format": "7.5",
