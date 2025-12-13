f72b8d75c2eee93a922121a5ad2277aff9ba052a
watermai-wm <akasukilee@gmail.com>
2025-12-13 19:00:57 +0800

    更新上位

--- topdeck.js
+++ topdeck.js
@@ -1,6 +1,15 @@
 // topdeck.js
 const topDecks = [
   {
+    "deck_id": "20251213-1",
+    "L_name": "哈爾濱",
+    "Format": "7.5.1",
+    "Description": "20251213 貓腳印-東門店 店家賽 5人 冠軍 : 哈爾濱 明石測牌器",
+    "Level": "常規賽",
+    "Players": 5,
+    "Champion": 1
+  },
+  {
     "deck_id": "20251209-1",
     "L_name": "哈爾濱",
     "Format": "7.5.1",
