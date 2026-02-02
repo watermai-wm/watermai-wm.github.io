eb4f379ba2e5484904b84f254ebbb1420f324db1
watermai-wm <akasukilee@gmail.com>
2026-02-02 21:03:22 +0800

    更新

--- topdeck.js
+++ topdeck.js
@@ -1,6 +1,15 @@
 // topdeck.js
 const topDecks = [
   {
+    "deck_id": "20260202-1",
+    "L_name": "沙恩霍斯特",
+    "Format": "7.5.1",
+    "Description": "20260202 玩具e哥花蓮和平店 碧藍戰卡CE週賽 4人(7.5.1) 冠軍 沙恩霍斯特 心皓",
+    "Level": "常規賽",
+    "Players": 4,
+    "Champion": 1
+  },
+  {
     "deck_id": "20260131-2",
     "L_name": "曙光",
     "Format": "7.5",
