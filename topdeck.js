075bafd7ed372c65cb928fc75871488831dd574b
watermai-wm <akasukilee@gmail.com>
2026-04-05 18:20:06 +0800

    更新上位

--- topdeck.js
+++ topdeck.js
@@ -1,6 +1,15 @@
 // topdeck.js
 const topDecks = [
   {
+    "deck_id": "20260405-1",
+    "L_name": "聖路易斯",
+    "Format": "7.5.1",
+    "Description": "20260405 貓腳印-士林店 店家賽 7人 冠軍 聖路易斯 男爵",
+    "Level": "常規賽",
+    "Players": 7,
+    "Champion": 1
+  },
+  {
     "deck_id": "20260402-1",
     "L_name": "武藏",
     "Format": "7.5.1",
