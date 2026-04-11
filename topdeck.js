8e2409f433fc014acbaf4c683d14b9452dba6ec3
watermai-wm <akasukilee@gmail.com>
2026-04-11 22:13:35 +0800

    更新金布里

--- topdeck.js
+++ topdeck.js
@@ -1,6 +1,15 @@
 // topdeck.js
 const topDecks = [
   {
+    "deck_id": "20260410-1",
+    "L_name": "曙光",
+    "Format": "7.5.1",
+    "Description": "20260410 玩具e哥花蓮和平店 碧藍戰卡CE週賽 8人(7.5.1) 冠軍 曙光 展示無敵的天羽羽斬",
+    "Level": "常規賽",
+    "Players": 8,
+    "Champion": 1
+  },
+  {
     "deck_id": "20260405-1",
     "L_name": "聖路易斯",
     "Format": "7.5.1",
