a221ffa2fad4dc6ca91f1ccd10f0c57f2ca1021e
watermai-wm <akasukilee@gmail.com>
2026-04-12 18:32:35 +0800

    更新上位

--- topdeck.js
+++ topdeck.js
@@ -1,6 +1,15 @@
 // topdeck.js
 const topDecks = [
   {
+    "deck_id": "20260412-1",
+    "L_name": "埃姆登",
+    "Format": "7.5.1",
+    "Description": "20260405 貓腳印-士林店 店家賽 6人 冠軍 埃姆登 現在我就是露娜sama",
+    "Level": "常規賽",
+    "Players": 6,
+    "Champion": 1
+  },
+  {
     "deck_id": "20260410-1",
     "L_name": "曙光",
     "Format": "7.5.1",
