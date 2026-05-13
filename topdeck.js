abfda84d1feb90c5bf378a2cac0a3ee88d6a475b
watermai-wm <akasukilee@gmail.com>
2026-05-13 21:18:10 +0800

    更新上位

--- topdeck.js
+++ topdeck.js
@@ -1,6 +1,15 @@
 // topdeck.js
 const topDecks = [
   {
+    "deck_id": "20260512-1",
+    "L_name": "鎮海",
+    "Format": "7.5.1",
+    "Description": "20260512 玩具e哥花蓮和平店 碧藍戰卡CE週賽 4人(7.5.1) 冠軍 鎮海 ひろし",
+    "Level": "常規賽",
+    "Players": 4,
+    "Champion": 1
+  },
+  {
     "deck_id": "20260510-1",
     "L_name": "怨仇",
     "Format": "7.5.1",
