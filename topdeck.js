0f4439348a98cb0d6e0a13347ef5bf66bd0fb6d4
watermai-wm <akasukilee@gmail.com>
2026-05-31 18:56:06 +0800

    更新上位

--- topdeck.js
+++ topdeck.js
@@ -1,13 +1,31 @@
 // topdeck.js
 const topDecks = [
   {
-    "deck_id": "20260530-3",
-    "L_name": "奧古斯特·馮·帕塞瓦爾",
-    "Format": "7.5.1",
-    "Description": "20260530 逛逛紙牌社 店家賽(7.5.1) 13人 亞軍：奧古斯特·馮·帕塞瓦爾",
+    "deck_id": "20260531-1",
+    "L_name": "蘇維埃俄羅斯",
+    "Format": "7.5.2",
+    "Description": "20260531 貓腳印-士林店 君主特殊賽(7.5.2) 8人 冠軍 蘇維埃俄羅斯 男爵",
     "Level": "常規賽",
-    "Players": 13,
+    "Players": 8,
+    "Champion": 1
+  },
+  {
+    "deck_id": "20260531-2",
+    "L_name": "列星頓",
+    "Format": "7.5.2",
+    "Description": "20260531 貓腳印-士林店 君主特殊賽(7.5.2) 8人 亞軍 列星頓 Whiskey",
+    "Level": "常規賽",
+    "Players": 8,
     "Champion": 0
+  },
+  {
+    "deck_id": "20260530-1",
+    "L_name": "腓特烈大帝",
+    "Format": "7.5.2",
+    "Description": "20260530 貓腳印-東門店 君主特殊賽(7.5.2) 4人 冠軍 腓特烈大帝 KIRA",
+    "Level": "常規賽",
+    "Players": 4,
+    "Champion": 1
   },
   {
     "deck_id": "20260530-2",
@@ -19,13 +37,13 @@ const topDecks = [
     "Champion": 1
   },
   {
-    "deck_id": "20260530-1",
-    "L_name": "腓特烈大帝",
-    "Format": "7.5.2",
-    "Description": "20260530 貓腳印-東門店 君主特殊賽(7.5.2) 4人 冠軍 腓特烈大帝 KIRA",
+    "deck_id": "20260530-3",
+    "L_name": "奧古斯特·馮·帕塞瓦爾",
+    "Format": "7.5.1",
+    "Description": "20260530 逛逛紙牌社 店家賽(7.5.1) 13人 亞軍：奧古斯特·馮·帕塞瓦爾",
     "Level": "常規賽",
-    "Players": 4,
-    "Champion": 1
+    "Players": 13,
+    "Champion": 0
   },
   {
     "deck_id": "20260528-1",
