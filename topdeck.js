5a1e994cf69b6c4365af1c8d975b292c5e22290c
watermai-wm <akasukilee@gmail.com>
2026-06-05 22:06:41 +0800

    更新上位

--- topdeck.js
+++ topdeck.js
@@ -1,6 +1,15 @@
 // topdeck.js
 const topDecks = [
   {
+    "deck_id": "20260605-1",
+    "L_name": "島風",
+    "Format": "7.5.2",
+    "Description": "20260605 玩具e哥花蓮和平店 碧藍戰卡CE週賽 8人(7.5.2) 冠軍 島風 ひろし",
+    "Level": "常規賽",
+    "Players": 8,
+    "Champion": 1
+  },
+  {
     "deck_id": "20260531-1",
     "L_name": "蘇維埃俄羅斯",
     "Format": "7.5.2",
@@ -21,7 +30,7 @@ const topDecks = [
   {
     "deck_id": "20260531-3",
     "L_name": "魯普雷希特親王",
-    "Format": "7.5.2",
+    "Format": "7.5.1",
     "Description": "20260531 屏東 交流賽(7.5.1) 4人 冠軍 魯普雷希特親王 金正恩",
     "Level": "常規賽",
     "Players": 4,
