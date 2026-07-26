fb49948570d8d653892221f7e7b6a8a93c3ddb90
watermai-wm <akasukilee@gmail.com>
2026-07-26 19:36:05 +0800

    更新上位

--- topdeck.js
+++ topdeck.js
@@ -1,6 +1,15 @@
 // topdeck.js
 const topDecks = [
   {
+    "deck_id": "20260726-1",
+    "L_name": "瑞鶴",
+    "Format": "7.5.2",
+    "Description": "20260726 貓腳印-士林店 碧藍戰卡CE週賽 6人(7.5.2) 冠軍 瑞鶴 男爵",
+    "Level": "常規賽",
+    "Players": 4,
+    "Champion": 1
+  },
+  {
     "deck_id": "20260722-1",
     "L_name": "曙光",
     "Format": "7.5.2",
