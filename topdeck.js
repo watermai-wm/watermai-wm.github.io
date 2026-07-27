454891edf6328205e46828eba3cfd51439f8e046
watermai-wm <akasukilee@gmail.com>
2026-07-27 12:14:27 +0800

    更新上位

--- topdeck.js
+++ topdeck.js
@@ -10,6 +10,15 @@ const topDecks = [
     "Champion": 1
   },
   {
+    "deck_id": "20260725-1",
+    "L_name": "瑞鶴",
+    "Format": "7.5.2",
+    "Description": "20260725 逛逛紙牌社 店家賽(7.5.2) 5人 冠軍 紐澤西",
+    "Level": "常規賽",
+    "Players": 5,
+    "Champion": 1
+  },
+  {
     "deck_id": "20260722-1",
     "L_name": "曙光",
     "Format": "7.5.2",
