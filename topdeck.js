150a31330fb525b24ae1082439d9e3eba371d1a7
watermai-wm <akasukilee@gmail.com>
2026-08-16 23:00:25 +0800

    更新上位

--- topdeck.js
+++ topdeck.js
@@ -1,6 +1,15 @@
 // topdeck.js
 const topDecks = [
   {
+    "deck_id": "20260816-1",
+    "L_name": "齊柏林伯爵",
+    "Format": "7.5.2",
+    "Description": "20260816 玩具e哥花蓮和平店 碧藍戰卡CE週賽 6人(7.5.2) 冠軍 歐根親王 心皓",
+    "Level": "常規賽",
+    "Players": 6,
+    "Champion": 1
+  },
+  {
     "deck_id": "20260811-1",
     "L_name": "齊柏林伯爵",
     "Format": "7.5.2",
